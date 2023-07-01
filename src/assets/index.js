3c08c70ded21d1db14020016c429f539aba84b8b
szymonaszek01 <235879@edu.p.lodz.pl>
2023-07-01 11:47:34 +0200

    task-004 [src] Adding images and icons

---
+++ src/assets/index.js
@@ -0,0 +1,53 @@
+import card from "./card.png";
+import localization from "./localization.png"
+import logo from "./logo.png";
+import quotes from "./quotes.svg";
+import hero from "./hero.png"
+import send from "./send.svg";
+import shield from "./shield.svg";
+import star from "./star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import email from "./email.png"
+import arrowUp from "./arrow-up.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import stripe from "./stripe.png"
+import room1 from "./room1.jpg"
+import room2 from "./room2.jpg"
+import room3 from "./room3.jpg"
+import room4 from "./room4.jpg"
+import twitter from "./twitter.svg";
+import telephone from "./telephone.png"
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+
+export {
+  card,
+  logo,
+  quotes,
+  hero,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  email,
+  arrowUp,
+  facebook,
+  instagram,
+  linkedin,
+  localization,
+  room1,
+  room2,
+  room3,
+  room4,
+  stripe,
+  twitter,
+  telephone,
+  people01,
+  people02,
+  people03
+}
