048bafed5dce90948d411273a1bdd39ccff52e7f
szymonaszek01 <235879@edu.p.lodz.pl>
2023-07-18 13:26:08 +0200

    task-005 [router] Handling routing

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,11 @@
-import {Routes, Route} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Layout from "../components/Layout";
 import HomePage from "../pages/public/home.page";
 import Authenticator from "../components/Autheticator";
+import SignInPage from "../pages/public/signIn.page";
+import OAuth2RedirectHandler from "../components/OAuth2RedirectHandler";
+import UserDashboardPage from "../pages/private/user-dashboard.page";
+import SessionExpiredPage from "../pages/public/sessionExpired.page";
 
 const Router = () => {
   return (
@@ -9,9 +13,13 @@ const Router = () => {
       <Route path="/" element={<Layout/>}>
         {/* public router */}
         <Route index element={<HomePage/>}/>
+        <Route path="/sign-in" element={<SignInPage/>}/>
+        <Route path="/sign-in/oauth2/callback" element={<OAuth2RedirectHandler/>}/>
+        <Route path="/session-expired" element={<SessionExpiredPage/>}/>
 
         {/* private router */}
         <Route element={<Authenticator/>}>
+          <Route path="/dashboard" element={<UserDashboardPage/>}/>
         </Route>
       </Route>
     </Routes>
